6555fbba6704818ce160539961d77f953dfcc919
Danyil Demianiuk <demyanyuk76@gmail.com>
2024-01-14 13:45:54 +0200

    added auth routes in server side

--- server/src/api/routes/auth.ts
+++ server/src/api/routes/auth.ts
@@ -61,161 +61,62 @@ export default (app: Router): void => {
     },
   );
 
-  //   route.post(
-  //     '/check',
-  //     celebrate({
-  //       body: Joi.object({
-  //         username: Joi.string().allow(''),
-  //         email: Joi.string()
-  //           .email({ tlds: { allow: false } })
-  //           .allow(''),
-  //       }),
-  //     }),
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const authServiceInstance = Container.get(AuthService);
-  //         await authServiceInstance.Check(req.body, req);
-  //         return res.status(201).json({ success: true });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
+  route.post(
+    '/check',
+    celebrate({
+      body: Joi.object({
+        name: Joi.string().allow(''),
+        email: Joi.string()
+          .email({ tlds: { allow: false } })
+          .allow(''),
+      }),
+    }),
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const authServiceInstance = Container.get(AuthService);
+        await authServiceInstance.Check(req.body, req);
+        return res.status(201).json({ success: true });
+      } catch (e) {
+        return next(e);
+      }
+    },
+  );
 
-  //   route.post(
-  //     '/demo',
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const authServiceInstance = Container.get(AuthService);
-  //         const { user, token, authType } = await authServiceInstance.SignIn(
-  //           'demo@soart.ai',
-  //           '11111111',
-  //         );
-  //         return res
-  //           .status(200)
-  //           .json({ success: true, data: { user, token, authType } });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
+  route.post(
+    '/update-password',
+    celebrate({
+      body: Joi.object({
+        currentPassword: Joi.string().required(),
+        newPassword: Joi.string().required(),
+      }),
+    }),
+    middlewares.isAuth,
+    middlewares.attachCurrentUser,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const { currentPassword, newPassword } = req.body;
+        const authServiceInstance = Container.get(AuthService);
+        const { user, token } = await authServiceInstance.UpdatePassword(
+          req.currentUser,
+          currentPassword,
+          newPassword,
+        );
+        return res.status(200).json({ success: true, data: { user, token } });
+      } catch (e) {
+        return next(e);
+      }
+    },
+  );
 
-  //   route.post(
-  //     '/apple',
-  //     celebrate({
-  //       body: Joi.object({
-  //         appleToken: Joi.string().required(),
-  //         appleUid: Joi.string().required(),
-  //         appleName: Joi.string().allow(''),
-  //       }),
-  //     }),
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const { appleToken, appleUid, appleName } = req.body;
-  //         const authServiceInstance = Container.get(AuthService);
-  //         const { user, token, authType } = await authServiceInstance.AppleAuth(
-  //           appleToken,
-  //           appleUid,
-  //           appleName,
-  //         );
-  //         return res
-  //           .status(200)
-  //           .json({ success: true, data: { user, token, authType } });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
-
-  //   route.post(
-  //     '/code',
-  //     celebrate({
-  //       body: Joi.object({
-  //         code: Joi.string().required(),
-  //       }),
-  //     }),
-  //     middlewares.isAuth,
-  //     middlewares.attachCurrentUser,
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const authServiceInstance = Container.get(AuthService);
-  //         await authServiceInstance.CheckCode(req.body.code, req.currentUser);
-  //         return res.status(200).json({ success: true });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
-
-  //   route.get(
-  //     '/code',
-  //     middlewares.isAuth,
-  //     middlewares.attachCurrentUser,
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const authServiceInstance = Container.get(AuthService);
-  //         await authServiceInstance.GetConfirmationCode(req.currentUser);
-  //         return res.status(200).json({ success: true });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
-
-  //   route.post(
-  //     '/update-password',
-  //     celebrate({
-  //       body: Joi.object({
-  //         currentPassword: Joi.string().required(),
-  //         newPassword: Joi.string().required(),
-  //       }),
-  //     }),
-  //     middlewares.isAuth,
-  //     middlewares.attachCurrentUser,
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const { currentPassword, newPassword } = req.body;
-  //         const authServiceInstance = Container.get(AuthService);
-  //         const { user, token } = await authServiceInstance.UpdatePassword(
-  //           req.currentUser,
-  //           currentPassword,
-  //           newPassword,
-  //         );
-  //         return res.status(200).json({ success: true, data: { user, token } });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
-
-  //   route.post(
-  //     '/reset-password',
-  //     celebrate({
-  //       body: Joi.object({
-  //         email: Joi.string().email({ tlds: { allow: false } }),
-  //       }),
-  //     }),
-  //     async (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         const { email } = req.body;
-  //         const authServiceInstance = Container.get(AuthService);
-  //         await authServiceInstance.ResetPassword(email);
-  //         return res.status(200).json({ success: true });
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
-
-  //   route.post(
-  //     '/logout',
-  //     middlewares.isAuth,
-  //     (req: Request, res: Response, next: NextFunction) => {
-  //       try {
-  //         return res.status(200).json({ success: true }).end();
-  //       } catch (e) {
-  //         return next(e);
-  //       }
-  //     },
-  //   );
+  route.post(
+    '/logout',
+    middlewares.isAuth,
+    (req: Request, res: Response, next: NextFunction) => {
+      try {
+        return res.status(200).json({ success: true }).end();
+      } catch (e) {
+        return next(e);
+      }
+    },
+  );
 };
--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,10 +1,7 @@
 import { Service, Inject, Container } from 'typedi';
-import jwt from 'jsonwebtoken';
 import { Document } from 'mongoose';
 import { AuthType, IUser, IUserInputDTO } from '../interfaces/IUser';
 import ErrorResponse from '../utils/errorResponse';
-//import generatePassword from '../utils/generatePassword';
-import config from '../config';
 
 @Service()
 export default class AuthService {
@@ -43,202 +40,51 @@ export default class AuthService {
     }
   }
 
-  // public async AppleAuth(
-  //   accessToken: string,
-  //   appleId: string,
-  //   appleName: string,
-  // ): Promise<{ user: IUser; token: string; authType?: AuthType }> {
-  //   const userRecordId = await this.userModel.findOne({ appleId });
-  //   const response = await this.appleAuth.accessToken(accessToken);
-  //   if (!response && !response.id_token) {
-  //     throw new ErrorResponse(
-  //       i18next.t('apple_authentication_fail', { lng: userRecordId?.language }),
-  //       401,
-  //     );
-  //   }
-  //   const idToken: any = jwt.decode(response.id_token);
-  //   if (!idToken && !idToken.email) {
-  //     throw new ErrorResponse(
-  //       i18next.t('apple_authentication_fail', { lng: userRecordId?.language }),
-  //       401,
-  //     );
-  //   }
-  //   if (userRecordId) {
-  //     if (userRecordId.status === Status.BLOCKED) {
-  //       throw new ErrorResponse('User blocked', 403);
-  //     }
-  //     if (userRecordId.email !== idToken.email) {
-  //       throw new ErrorResponse(
-  //         i18next.t('apple_authentication_fail', {
-  //           lng: userRecordId?.language,
-  //         }),
-  //         401,
-  //       );
-  //     }
-  //     return this.getToken(userRecordId, AuthType.SIGNIN);
-  //   }
-  //   let userRecordEmail = await this.userModel.findOne({
-  //     email: idToken.email,
-  //   });
-  //   if (userRecordEmail) {
-  //     userRecordEmail = await this.userModel.findOneAndUpdate(
-  //       { email: idToken.email },
-  //       { appleId },
-  //       { new: true },
-  //     );
-  //     return this.getToken(userRecordEmail, AuthType.SIGNIN);
-  //   }
-  //   let username = '';
-  //   if (appleName) {
-  //     const normalizedUsername = normalizeUsername(appleName);
-  //     const userRecordName = await this.userModel.findOne({
-  //       username: normalizedUsername,
-  //     });
-  //     if (userRecordName) {
-  //       username = normalizedUsername + Date.now();
-  //     } else {
-  //       username = normalizedUsername;
-  //     }
-  //   } else {
-  //     username = 'user_' + Date.now();
-  //   }
-  //   const userRecord = await this.createUser({
-  //     username,
-  //     email: idToken.email,
-  //     appleId,
-  //     isEmailConfirmed: true,
-  //     status: Status.ACTIVE,
-  //   });
-  //   return this.getToken(userRecord, AuthType.SIGNUP);
-  // }
+  public async Check(userInputDTO: IUserInputDTO, req: any): Promise<void> {
+    if (userInputDTO.email === '') {
+      throw new ErrorResponse('Email cannot be empty!', 401);
+    }
+    if (userInputDTO.name === '') {
+      throw new ErrorResponse('Name cannot be empty!', 401);
+    }
+    if (userInputDTO.email) {
+      const email = userInputDTO.email.toLowerCase();
+      const isEmail = await this.userModel.findOne({ email });
+      if (isEmail) {
+        throw new ErrorResponse('Email already exist!', 401);
+      }
+    }
+    if (userInputDTO.name) {
+      const isUsername = await this.userModel.findOne({
+        name: userInputDTO.name,
+      });
+      if (isUsername) {
+        throw new ErrorResponse('Name already exist!', 401);
+      }
+    }
+  }
 
-  // public async Check(userInputDTO: IUserInputDTO, req: any): Promise<void> {
-  //   if (userInputDTO.email === '') {
-  //     throw new ErrorResponse(req.t('not_empty_email'), 401, 'email');
-  //   }
-  //   if (userInputDTO.username === '') {
-  //     throw new ErrorResponse(req.t('not_empty_username'), 401, 'username');
-  //   }
-  //   if (userInputDTO.email) {
-  //     const email = userInputDTO.email.toLowerCase();
-  //     const isEmail = await this.userModel.findOne({ email });
-  //     if (isEmail) {
-  //       throw new ErrorResponse(req.t('email_already_exists'), 401, 'email');
-  //     }
-  //   }
-  //   if (userInputDTO.username) {
-  //     const isUsername = await this.userModel.findOne({
-  //       username: userInputDTO.username,
-  //     });
-  //     if (isUsername) {
-  //       throw new ErrorResponse(
-  //         req.t('username_already_exsists'),
-  //         401,
-  //         'username',
-  //       );
-  //     }
-  //   }
-  // }
+  public async UpdatePassword(
+    currentUser: IUser,
+    currentPassword: string,
+    newPassword: string,
+  ): Promise<{ user: IUser; token: string }> {
+    const userRecord = await this.userModel
+      .findById(currentUser._id)
+      .select('+password');
+    if (!userRecord) {
+      throw new ErrorResponse('The user is not registered', 400);
+    }
 
-  // public async CheckCode(code: string, userRecord: IUser): Promise<void> {
-  //   const confirmationCodeToken = crypto
-  //     .createHash('sha256')
-  //     .update(code)
-  //     .digest('hex');
-  //   const user = await this.userModel.findOne({
-  //     _id: userRecord._id,
-  //     confirmationCodeToken,
-  //     confirmationCodeExpire: { $gt: Date.now() },
-  //   });
-
-  //   if (!user) {
-  //     throw new ErrorResponse(
-  //       i18next.t('invalid_code', { lng: userRecord?.language }),
-  //       400,
-  //     );
-  //   }
-  //   user.confirmationCodeToken = undefined;
-  //   user.confirmationCodeExpire = undefined;
-  //   user.isEmailConfirmed = true;
-  //   user.status = Status.ACTIVE;
-  //   await user.save();
-  // }
-
-  // public async GetConfirmationCode(userRecord: IUser): Promise<void> {
-  //   const user = await this.userModel.findById(userRecord._id);
-  //   if (user) {
-  //     await this.SendConfirmationCode(user);
-  //   }
-  // }
-
-  // public async UpdatePassword(
-  //   currentUser: IUser,
-  //   currentPassword: string,
-  //   newPassword: string,
-  // ): Promise<{ user: IUser; token: string }> {
-  //   const userRecord = await this.userModel
-  //     .findById(currentUser._id)
-  //     .select('+password');
-  //   if (!userRecord) {
-  //     throw new ErrorResponse(
-  //       i18next.t('user_not_registered', { lng: currentUser?.language }),
-  //       400,
-  //     );
-  //   }
-  //   if (userRecord.status === Status.DELETED) {
-  //     throw new ErrorResponse(
-  //       i18next.t('user_deleted', { lng: currentUser?.language }),
-  //       400,
-  //     );
-  //   }
-  //   if (userRecord.status === Status.BLOCKED) {
-  //     throw new ErrorResponse('User blocked', 400);
-  //   }
-  //   const validPassword = await userRecord.matchPassword(currentPassword);
-  //   if (validPassword) {
-  //     userRecord.password = newPassword;
-  //     await userRecord.save();
-  //     return this.getToken(userRecord);
-  //   } else {
-  //     throw new ErrorResponse(
-  //       i18next.t('incorrect_password', { lng: currentUser?.language }),
-  //       400,
-  //     );
-  //   }
-  // }
-
-  // public async ResetPassword(email: string): Promise<boolean> {
-  //   const userRecord = await this.userModel.findOne({ email });
-  //   if (!userRecord) {
-  //     throw new ErrorResponse(
-  //       i18next.t('user_not_registered', { lng: userRecord?.language }),
-  //       400,
-  //     );
-  //   }
-  //   if (userRecord.status === Status.DELETED) {
-  //     throw new ErrorResponse(
-  //       i18next.t('user_deleted', { lng: userRecord?.language }),
-  //       400,
-  //     );
-  //   }
-  //   if (userRecord.status === Status.BLOCKED) {
-  //     throw new ErrorResponse('User blocked', 400);
-  //   }
-  //   const newPassword = generatePassword({ length: 8 });
-  //   userRecord.password = newPassword;
-  //   await userRecord.save();
-  //   const mailServiceInstance = Container.get(MailService);
-  //   const result = await mailServiceInstance.SendEmail({
-  //     email,
-  //     name: userRecord.username,
-  //     template: TemplateType.resetPassword,
-  //     actionData: {
-  //       newPassword,
-  //       user: userRecord,
-  //     },
-  //   });
-  //   return result;
-  // }
+    const validPassword = await userRecord.matchPassword(currentPassword);
+    if (validPassword) {
+      userRecord.password = newPassword;
+      await userRecord.save();
+      return this.getToken(userRecord);
+    } else {
+      throw new ErrorResponse('The password is incorrect!', 400);
+    }
+  }
 
   private async createUser(userData: IUserInputDTO): Promise<IUser & Document> {
     const userRecord = await this.userModel.create({
@@ -247,15 +93,6 @@ export default class AuthService {
 
     return userRecord;
   }
-
-  // private async SendConfirmationCode(
-  //   userRecord: IUser & Document,
-  // ): Promise<void> {
-  //   const code = userRecord.getConfirmationCode();
-  //   await userRecord.save({ validateBeforeSave: false });
-  //   const mailServiceInstance = Container.get(MailService);
-  //   await mailServiceInstance.SendConfirmationCode(userRecord, code);
-  // }
 
   private async getToken(
     userRecord: IUser & Document,
